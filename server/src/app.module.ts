6255fdc3918fdcce963a218cb059f7f45dd0ded5
phat181999 <phat181999@gmail.com>
2024-06-21 22:36:35 +0700

    create structure for server

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,11 +1,40 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
+import { RouterModule, Routes } from '@nestjs/core';
+// import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
+const routes: Routes = [
+  {
+    path: '/api',
+    children: [
+      {
+        path: '/users',
+        // module: UsersModule,
+      },
+    ],
+  },
+];
+console.log(process.env.HOST);
 @Module({
-  imports: [UsersModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    RouterModule.register(routes),
+    ConfigModule.forRoot({
+      envFilePath: '.development.env',
+      cache: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.HOST,
+      port: 5432,
+      database: process.env.POSTGRES_DB,
+      username: process.env.USERNAMEDB,
+      password: process.env.PASSWORDDB,
+      entities: [],
+      synchronize: true,
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
